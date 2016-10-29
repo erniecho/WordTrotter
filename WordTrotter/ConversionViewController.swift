b7a79d3960edfc9f3d724fd387b4ebb7bdb40efd
erniecho <erniecho@gmail.com>
2016-10-29 18:26:50 -0400

    remove a swift file that does all the calculations.

---
+++ WordTrotter/ConversionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionViewController.swift
+//  WordTrotter
+//
+//  Created by Ernie Cho on 10/29/16.
+//  Copyright © 2016 ErnShu. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewControler: UIViewController {
+
+}
